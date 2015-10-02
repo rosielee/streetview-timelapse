c2ab033926e331a855945aa5422b2f913e133038
Ed Williams <edwilliamsdevelopment@gmail.com>
2015-10-02 11:45:11 -0400

    edit; port

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use('/', home);
 // start server
 // ========================================
 
-var server = app.listen(3000, function () {
+var server = app.listen(3001, function () {
     var host = server.address().address;
     var port = server.address().port;
 
